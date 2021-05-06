20e21c7eb816e38acc2b7d38dda05f5ff90ce379
Nicole Canet <nicole.canet.porras@est.una.ac.cr>
2021-05-05 22:34:17 -0600

    Register

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -1,26 +1,74 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../database')
+const jwt = require('jsonwebtoken');
+
+// Authorization: Bearer <token>
+function ensureToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (bearerHeader === undefined) {
+        res.redirect('/api/not_allowed');
+    } else {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    }
+}
 
 router.get('/', (req, res) => {
     res.render('index');
 });
 
 router.get('/login', (req, res) => {
-    db.query("call obtener_usuario(?,?)", [req.query.username,req.query.password], (error,rows,fields)=>{
-        if(!error){
-            req.session.user = rows[0];
-            let user = req.session.user;
-            res.render('sesion', { user });
-        }else{
-            res.send({status: 500})
+    db.query("call obtener_usuario(?,?)", [req.query.username, req.query.password], (error, rows, fields) => {
+        if (!error) {
+            let user = rows[0];
+            jwt.sign({ user }, 'secretKeyToken', (err, token) => {
+                res.render('sesion', { user, token });
+            });
+        } else {
+            res.send({ status: 500 })
         }
     })
 });
 
-router.get('/sesion', (req, res) => {
+router.get('/sesion', ensureToken, (req, res) => {
     let user = req.session.usuario;
+    var decoded = jwt.decode(req.token);
+    // get the decoded payload and header
+    var decoded = jwt.decode(req.token, { complete: true });
+    console.log(decoded.payload);
     res.render('sesion', { user });
+});
+
+router.get('/register/show', (req, res) => {
+    res.render('register');
+});
+
+router.post('/register/insert', (req, res) => {
+
+    db.query("call insertar_usuario(?,?,?)", [req.body.email, req.body.name, req.body.password], (error, rows, fields) => {
+        if (!error) {
+            let user = {
+                name: req.body.name,
+                email: req.body.email
+            }
+            console.log(user)
+            req.session.usuario = user;
+            console.log(req.session.usuario)
+            res.send({ status: 200 })
+        } else {
+            res.send({ status: 500 })
+        }
+    })
+});
+
+router.get('/home/show', (req, res) => {
+    let user = req.session.usuario;
+    console.log(user)
+    res.render('home', { user });
+
 });
 
 module.exports = router;
